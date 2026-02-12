cbebb84c5bfc896427d2325e95a3be2b60ddfb52
Bogdan Otava <bogdanotava097@gmail.com>
2026-02-12 15:29:44 +0200

    Added - PSet2/deep.py

---
+++ PSet_2/deep.py
@@ -0,0 +1,20 @@
+def main():
+    question = input(
+        "What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+    if check_answer(question):
+        print("Yes")
+    else:
+        print("No")
+
+
+def check_answer(query: str):
+    """Takes a string as argument and returns a boolean value."""
+
+    # Makes the argument lower case and strips all the leading/trailing whitespaces.
+    query = query.lower().strip()
+
+    return query == "42" or query == "forty two" or query == "forty-two"
+
+
+main()
