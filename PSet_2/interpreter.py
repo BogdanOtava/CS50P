2997cd753e14044f6aa115215e27d8154bd049b3
Bogdan Otava <bogdanotava097@gmail.com>
2026-02-14 15:27:19 +0200

    Added - PSet2/interpreter.py

---
+++ PSet_2/interpreter.py
@@ -0,0 +1,30 @@
+def main():
+    expression = input("Expression: ")
+
+    print(calculate_expression(expression))
+
+
+def calculate_expression(query: str):
+    """Calculates two values by the given operator."""
+
+    # Splits the input into a list of strings.
+    x, y, z = query.split()
+
+    # Convert the operands into integers.
+    x = int(x)
+    z = int(z)
+
+    # Check the type of operator
+    if y == "+":
+        result = x + z
+    elif y == "-":
+        result = x - z
+    elif y == "*":
+        result = x * z
+    else:
+        result = x / z
+
+    return float(result)
+
+
+main()
