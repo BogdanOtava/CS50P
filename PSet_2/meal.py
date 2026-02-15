bb16bfa7ecb2dc578a797c2fc6fbd60478e8dc0f
Bogdan Otava <bogdanotava097@gmail.com>
2026-02-15 18:01:05 +0200

    Added - PSet2/meal.py

---
+++ PSet_2/meal.py
@@ -0,0 +1,53 @@
+def main():
+    query = input("What time is it? ")
+
+    result = convert(query)
+
+    if 7 <= result <= 8:
+        print("breakfast time")
+    elif 12 <= result <= 13:
+        print("lunch time")
+    elif 18 <= result <= 19:
+        print("dinner time")
+
+
+def convert(time: str):
+    """Converts 12-hour and 24-hour time formats into a floating point value."""
+
+    time_format = None
+    time = time.lower().strip()
+
+    # Checks whether the input endswith 'am' or 'pm', remove the suffix and record the format.
+    if time.endswith("a.m."):
+        time = time[:-5]
+        time_format = "am"
+    elif time.endswith("am"):
+        time = time[:-2]
+        time_format = "am"
+    elif time.endswith("p.m."):
+        time = time[:-5]
+        time_format = "pm"
+    elif time.endswith("pm"):
+        time = time[:-2]
+        time_format = "pm"
+
+    # Splits the string into hour and minutes.
+    hour, minutes = time.split(":")
+
+    hour = int(hour)
+    minutes = int(minutes)
+
+    # Converts 12-hour format into 24-hour format if needed.
+    if time_format == "am":
+        if hour == 12:
+            hour = 0
+    elif time_format == "pm":
+        if hour != 12:
+            hour += 12
+
+    # Return as a float representing the hour.
+    return hour + minutes / 60
+
+
+if __name__ == "__main__":
+    main()
